f03d6ed3d3867dc040a6a0bbfec64fd338136ac1
Victor Kean <vickean@gmail.com>
2015-08-28 14:54:27 +0800

    Corrected 'pos' to 'starting_position'
    Changed 'pos' to 'starting_position' in line 26 which defines said variable as 'Global'.

--- adventuretutorial/world.py
+++ adventuretutorial/world.py
@@ -23,7 +23,7 @@ def load_tiles():
         for x in range(x_max):
             tile_name = cols[x].replace('\n', '')
             if tile_name == 'StartingRoom':
-                global pos
+                global starting_position
                 starting_position = (x, y)
             _world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
 
